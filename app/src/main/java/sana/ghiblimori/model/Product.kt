3d329181d1aaa8075d55720e5af7b4a4665b1c2f
sana <sanalayouni41.@gmail.com>
2026-01-05 02:41:42 +0100

    add the category model

---
+++ app/src/main/java/sana/ghiblimori/model/Product.kt
@@ -0,0 +1,10 @@
+package sana.ghiblimori.model
+
+data class Product(
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
+    val imageUrl: String = "",
+    val categoryId: String = ""
+)
